bf64b2b73a3fa32b48120889b4e564cf16d25a21
Saikiran27112002 <saikiranvanthatipalli@gmail.com>
2025-03-21 14:40:54 +0530

    Add files via upload

---
+++ saikiran.py1.py
@@ -0,0 +1,26 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=25
+>>> b=35
+>>> add=a+b
+>>> sub=a-b
+>>> mul=a*b
+>>> div=a/b
+>>> mod=a%b
+>>> fd=a//b
+>>> ex=a**b
+>>> print(add)
+60
+>>> print(sub)
+-10
+>>> print(mul)
+875
+>>> print(div)
+0.7142857142857143
+>>> print(mod)
+25
+>>> print(fd)
+0
+>>> print(ex)
+8470329472543003390683225006796419620513916015625
+>>> 
