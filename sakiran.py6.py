2c5476d067f4cca2c83b6d44114c92da7b3f921f
Saikiran27112002 <saikiranvanthatipalli@gmail.com>
2025-03-21 14:46:03 +0530

    Add files via upload

---
+++ sakiran.py6.py
@@ -0,0 +1,5 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Administrator/AppData/Local/Programs/Python/Python37/saikiran.py6.py
+enter diameter of a circle
