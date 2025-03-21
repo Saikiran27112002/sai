a0ccb4851a8caf4ac1e4146f290f95e59734af85
Saikiran27112002 <saikiranvanthatipalli@gmail.com>
2025-03-21 14:45:21 +0530

    Add files via upload

---
+++ saikiran.py5.py
@@ -0,0 +1,8 @@
+l=int(input("enter length of a rectangle"))
+w=int(input("enter width of a rectangle"))
+area=l*w
+perimeter=2*(l+w)
+print("the area of a rectangle is",area)
+print("the perimeter of a rectangle is",perimeter)
+
+      
